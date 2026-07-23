630c5f2a47288d0f01123677b80042dd501d9895
Sumit Singh <125194525+sumit9794@users.noreply.github.com>
2026-07-23 11:12:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,7 @@ function App() {
         </div>
         <div>
           <h1>My First React AWS Hosted With Docker</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+        
         </div>
         <button
           type="button"
